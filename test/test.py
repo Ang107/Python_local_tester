88e4b34de801bc8c570045471527212e82258f35
Ang107 <kosukenabeta1007@gmail.com>
2024-06-05 19:13:00 +0900

    　入出力の要約。ドキュメントの改良。

--- test/test.py
+++ test/test.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 
 
 def select_problem() -> int:
@@ -23,6 +22,31 @@ def select_problem() -> int:
         except ValueError:
             print(f"入力が不正です。適切な入力の一覧: {expected_problem_num}")
     return problem_num
+
+
+def summarize_output(output: str, max_lines: int = 10, max_length: int = 50) -> str:
+    """
+    長い出力を要約して表示するための関数。
+    各行の長さが指定された最大長を超える場合、最初と最後の5文字を表示する。
+    行数が指定された最大行数を超える場合、最初の25行と最後の25行を表示する。
+    """
+    lines = output.splitlines()
+
+    def shorten_line(line: str, max_length: int) -> str:
+        if len(line) > max_length:
+            return f"{line[:max_length//2]}\033[90m...skip {len(line) - max_length}chars...\033[0m{line[-max_length//2:]}"
+        return line
+
+    if len(lines) > max_lines:
+        summarized_lines = (
+            [shorten_line(line, max_length) for line in lines[: max_lines // 2]]
+            + [f"\033[90m... skip {len(lines) - max_lines} lines ...\033[0m"]
+            + [shorten_line(line, max_length) for line in lines[-max_lines // 2 :]]
+        )
+    else:
+        summarized_lines = [shorten_line(line, max_length) for line in lines]
+
+    return "\n".join(summarized_lines)
 
 
 def run_test(problem_num: int, timeout: float) -> tuple[int, int, list[str]]:
@@ -56,12 +80,17 @@ def run_test(problem_num: int, timeout: float) -> tuple[int, int, list[str]]:
             stdout, stderr = process.communicate(input=test_input, timeout=timeout)
             passed = stdout.strip() == expected_output
             if stderr:
-                result = f"\033[91mRE\033[0m\n{stderr}\n"  # 赤色
+                result = f"\033[91mRE\033[0m\n{summarize_output(stderr)}\n"  # 赤色
             elif passed:
                 result = "\033[92mAC\033[0m\n"  # 緑色
                 AC_num += 1
             else:
-                result = f"\033[91mWA\033[0m\n\033[96m[in]\033[0m:\n{test_input}\033[96m[out]\033[0m:\n{stdout}\033[96m[expected_out]\033[0m:\n{expected_output}\n"  # 赤色 + シアン色
+                result = (
+                    f"\033[91mWA\033[0m\n"
+                    f"\033[96m[in]\033[0m:\n{summarize_output(test_input)}\n"
+                    f"\033[96m[out]\033[0m:\n{summarize_output(stdout)}\n"
+                    f"\033[96m[expected_out]\033[0m:\n{summarize_output(expected_output)}\n"
+                )  # 赤色 + シアン色
         except subprocess.TimeoutExpired:
             process.kill()
             result = "\033[91mTLE\033[0m\n"  # 赤色
